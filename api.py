6eae2d7dcd34a08fc98165a5a7452e8512c6d6aa
Akila Wasalathilaka <akilawasalathilaka@gmail.com>
2025-05-27 19:04:10 +0530

    update api.py

--- api.py
+++ api.py
@@ -144,4 +144,5 @@ def health_check():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='localhost', port=8000)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=False, host='0.0.0.0', port=port)
